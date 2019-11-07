62459e63b3548e7ef2a9a909ff836be4e070762c
Zhang <mzhang2@paypal.com>
2019-11-07 18:16:10 +0800

    [new] Cons added to ADT (with test)

---
+++ adt/cons.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from functools import reduce
+from typing import Tuple, NamedTuple, Any, Optional, Iterator, Iterable
+
+
+class Cons(NamedTuple):
+    """
+    Immutable Linked List
+    """
+    car: Any
+    cdr: Cons = None
+
+    def traversal(self) -> Iterator[Any]:
+        rec = self
+        while rec is not None:
+            yield rec.car
+            rec = rec.cdr
+
+    @classmethod
+    def constr_reverse(cls, it: Iterator[Any]):
+        return reduce(lambda x1, x2: cls(x2, x1), it, None)
+
+    def __reversed__(self):
+        return (type(self)).constr_reverse(self.traversal())
+
+    @classmethod
+    def constr(cls, it: Iterator[Any]):
+        return cls.constr_reverse(it).__reversed__()
---
+++ adt/tests/test_cons.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""test for ADT.cons
+
+"""
+
+import unittest
+from adt.cons import Cons
+
+
+class ConsTest(unittest.TestCase):
+    def test_traversal(self):
+        c1 = Cons(1, Cons(2, Cons(3, Cons(4))))
+        c1_traversal = (1, 2, 3, 4)
+        self.assertEqual(tuple(c1.traversal()), c1_traversal)
+
+    def test_constr(self):
+        c1_tuple = (2, 4, 6, 0, 1)
+        c1 = Cons(2, Cons(4, Cons(6, Cons(0, Cons(1)))))
+        self.assertEqual(Cons.constr(c1_tuple), c1)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)

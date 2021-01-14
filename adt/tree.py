78a546e206f2c4cb8577b83630a0b83aa94c63bd
ppmzhang2 <50707839+ppmzhang2@users.noreply.github.com>
2021-01-14 16:10:56 +0800

    [fix] tree ADT revised & other fixes (#2)
    * [new] new method for tree's depth-first search
    
    * [new] in-order depth-first search for B-Tree
    
    * [fix] 'type(self)' instead of hard-coded class

--- adt/tree.py
+++ adt/tree.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
+
 from functools import reduce
-from typing import Tuple, NamedTuple, Any, Optional
+from itertools import chain
+from typing import Any, Callable, Iterable, List, NamedTuple, Optional, Tuple
+
+from adt.stack import Stack
 
 
 class Tree(NamedTuple):
@@ -11,25 +15,43 @@ class Tree(NamedTuple):
     node: Any
     children: Tuple[Tree, ...] = ()
 
+    @property
+    def is_leaf(self):
+        return self.children == ()
+
+    @classmethod
+    def _dfs_helper(cls, stack: Stack, acc: Tuple[Any, ...],
+                    cb: Callable[[Tree], Iterable[Tree]]):
+        """depth-first search helper
+        depth-first search is like manipulating a stack: get a tree in a stack,
+        then pop it out and push into the stack its node value and its children
+        (the order is depending on whether this is a pre-order or post-order
+        search). Repeat this step until the stack is empty
+
+        :param stack: stack to keep the tree
+        :param acc: sequence of the search result
+        :param cb: callback function to arrange the node and the children of
+          the tree popped out
+        """
+        while True:
+            if stack.empty():
+                break
+
+            tree: Tree = stack.pop()
+            if tree.is_leaf:
+                acc = acc + (tree.node, )
+            else:
+                items = cb(tree)
+                for child in items:
+                    stack.push(child)
+
+        return acc
+
     @staticmethod
     def __bfs(tp: Tuple[Tree, ...]) -> Tuple[Tree, ...]:
         return reduce(
             lambda x, y: x + tuple(
                 filter(lambda tr: tr is not None, y.children)), tp, ())
-
-    @staticmethod
-    def __dfs_pre(tp: Tuple[Tree, ...]) -> Tuple[Tree, ...]:
-        tr = tp[0]
-        return tuple(
-            filter(lambda t: t is not None,
-                   (Tree(node=tr.node), ) + tr.children)) + tp[1:]
-
-    @staticmethod
-    def __dfs_post(tp: Tuple[Tree, ...]) -> Tuple[Tree, ...]:
-        tr = tp[0]
-        return tuple(
-            filter(lambda t: t is not None, tr.children +
-                   (Tree(node=tr.node), ))) + tp[1:]
 
     def bfs(self) -> Tuple[Any, ...]:
         """Breadth-first search
@@ -46,23 +68,19 @@ class Tree(NamedTuple):
 
         return helper((self, ), ())
 
-    def __dfs(self, cb):
-        def helper(tp: Tuple[Tree, ...],
-                   rec: Tuple[Any, ...]) -> Tuple[Any, ...]:
-            if not tp:
-                return rec
-            elif tp[0].children == ():
-                return helper(tp[1:], rec + (tp[0].node, ))
-            else:
-                return helper(cb(tp), rec)
-
-        return helper((self, ), ())
-
     def dfs_pre(self) -> Tuple[Any, ...]:
-        return self.__dfs(Tree.__dfs_pre)
+        def callback(tree: Tree):
+            return chain(reversed(tree.children),
+                         (type(self)(node=tree.node), ))
+
+        return self._dfs_helper(Stack((self, )), (), callback)
 
     def dfs_post(self) -> Tuple[Any, ...]:
-        return self.__dfs(Tree.__dfs_post)
+        def callback(tree: Tree):
+            return chain((type(self)(node=tree.node), ),
+                         reversed(tree.children))
+
+        return self._dfs_helper(Stack((self, )), (), callback)
 
     def depth(self) -> int:
         """check binary tree depth with depth-first search
@@ -90,24 +108,9 @@ class BTree(Tree):
         self.right = right
         return self
 
-    @staticmethod
-    def __dfs_in(tp: Tuple[BTree, ...]) -> Tuple[BTree, ...]:
-        bt = tp[0]
-        return tuple(
-            filter(lambda tr: tr is not None,
-                   (bt.left, BTree(node=bt.node), bt.right))) + tp[1:]
-
-    def __dfs(self, cb):
-        def helper(tp: Tuple[BTree, ...],
-                   rec: Tuple[Any, ...]) -> Tuple[Any, ...]:
-            if not tp:
-                return rec
-            elif tp[0].left is None and tp[0].right is None:
-                return helper(tp[1:], rec + (tp[0].node, ))
-            else:
-                return helper(cb(tp), rec)
-
-        return helper((self, ), ())
-
     def dfs_in(self) -> Tuple[Any, ...]:
-        return self.__dfs(BTree.__dfs_in)
+        def callback(tree: BTree):
+            return filter(lambda t: t is not None,
+                          (tree.right, type(self)(node=tree.node), tree.left))
+
+        return self._dfs_helper(Stack((self, )), (), callback)

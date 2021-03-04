552b999f4be0dd8fcb95a852b7f4b25c00a293c1
ZHANG, Meng <ztz2000@gmail.com>
2021-03-04 12:33:09 +0800

    [new] medium quiz 80 90 102 103 105 added

--- src/m0090.py
+++ src/m0090.py
@@ -17,16 +17,69 @@ Output:
   []
 ]
 """
-# TODO
-from typing import Sequence
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import IntEnum, unique
+from typing import Sequence, Tuple
+
+
+@unique
+class State(IntEnum):
+    DOING = 0
+    DONE = 1
+
+
+@dataclass(frozen=True)
+class FSM:
+    seq: Tuple[int, ...]
+    remaining: Tuple[int, ...]
+
+    @property
+    def state(self):
+        if not self.remaining:
+            return State.DONE
+        return State.DOING
+
+    def explode(self) -> Tuple[FSM, FSM]:
+        assert self.state == State.DOING
+        value = self.remaining[0]
+        fsm_new = (type(self))((*self.seq, value), self.remaining[1:])
+        fsm_old = (type(self))(self.seq, self.remaining[1:])
+        return fsm_old, fsm_new
+
+
+class Subset2:
+    @classmethod
+    def looper(
+        cls,
+        inputs: Tuple[FSM, ...],
+        outputs: Tuple[Tuple[int, ...], ...],
+    ) -> Tuple[Tuple[int, ...], ...]:
+        if not inputs:
+            return outputs
+        fsm = inputs[0]
+        if fsm.state == State.DONE:
+            return cls.looper(inputs[1:], (*outputs, fsm.seq))
+        return cls.looper((*fsm.explode(), *inputs[1:]), outputs)
+
+    def solution(self, seq: Sequence[int]):
+        fsm = FSM((), seq)
+        raw_seq = self.looper((fsm, ), ())
+        unique_seq = set(raw_seq)
+        return tuple(sorted(unique_seq))
+
 
 if __name__ == '__main__':
     ipt_1 = [1, 2, 2]
     exp_1 = (
-        (2, ),
-        (1, ),
-        (1, 2, 2),
-        (2, 2),
-        (1, 2),
         (),
+        (1, ),
+        (1, 2),
+        (1, 2, 2),
+        (2, ),
+        (2, 2),
     )
+
+    subset = Subset2()
+    assert subset.solution(ipt_1) == exp_1
--- src/m0102.py
+++ src/m0102.py
@@ -16,3 +16,98 @@ return its level order traversal as:
   [15,7]
 ]
 """
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import IntEnum, unique
+from typing import Optional, Tuple
+
+
+@unique
+class State(IntEnum):
+    TREE = 1
+    LEAF = 2
+
+
+@dataclass(frozen=True)
+class BTree:
+    root: int
+    left: Optional[BTree] = None
+    right: Optional[BTree] = None
+
+    def explode(self) -> Tuple[BTree, ...]:
+        if not self.left and not self.right:
+            return ()
+        if not self.left:
+            return (self.right, )
+        if not self.right:
+            return (self.left, )
+        return (self.left, self.right)
+
+
+@dataclass(frozen=True)
+class BTreePlus:
+    tree: BTree
+    level: int
+
+    def explode(self) -> Tuple[BTreePlus, ...]:
+        seq_tree = self.tree.explode()
+        level = self.level + 1
+        return tuple((type(self))(tr, level) for tr in seq_tree)
+
+    @property
+    def state(self):
+        if not self.tree.left and not self.tree.right:
+            return State.LEAF
+        return State.TREE
+
+
+@dataclass(frozen=True)
+class Node:
+    value: int
+    level: int
+
+
+class LevelOrderTraverse:
+    @classmethod
+    def looper(
+        cls,
+        inputs: Tuple[BTreePlus, ...],
+        outputs: Tuple[Node, ...],
+    ) -> Tuple[Node, ...]:
+        if not inputs:
+            return outputs
+        bt = inputs[0]
+        if bt.state == State.LEAF:
+            return cls.looper(
+                inputs[1:],
+                (*outputs, Node(bt.tree.root, bt.level)),
+            )
+        return cls.looper(
+            (*inputs[1:], *bt.explode()),
+            (*outputs, Node(bt.tree.root, bt.level)),
+        )
+
+    def solution(self, btree: BTree):
+        bt = BTreePlus(btree, 0)
+        seq_node = self.looper((bt, ), ())
+        res_seq = []
+        level_seq = []
+        level = 0
+        for node in seq_node:
+            if node.level != level:
+                res_seq.append(level_seq)
+                level += 1
+                level_seq = []
+            level_seq.append(node.value)
+        res_seq.append(level_seq)
+
+        return res_seq
+
+
+if __name__ == '__main__':
+    ipt_1 = BTree(3, BTree(9), BTree(20, BTree(15), BTree(7)))
+    exp_1 = [[3], [9, 20], [15, 7]]
+
+    lot = LevelOrderTraverse()
+    assert lot.solution(ipt_1) == exp_1

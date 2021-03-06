99feb7ae96d5223c95ceb620650fc50303c217d2
ZHANG, Meng <ztz2000@gmail.com>
2021-03-06 21:52:55 +0800

    [new] medium quiz 73 98 105; binary tree re-construct

--- src/m0098.py
+++ src/m0098.py
@@ -1,28 +1,100 @@
-"""Validate Binary Search Tree
-TAG: dynamic-programming
+from __future__ import annotations
 
-Given the root of a binary tree, determine if it is a valid binary search tree
-(BST).
+from dataclasses import dataclass
+from enum import IntEnum, unique
+from typing import Optional, Tuple
 
-A valid BST is defined as follows:
 
-* The left subtree of a node contains only nodes with keys less than the node's
-  key.
-* The right subtree of a node contains only nodes with keys greater than the node's
-  key.
-* Both the left and right subtrees must also be binary search trees.
+@unique
+class Position(IntEnum):
+    ROOT = 0
+    LEFT = 1
+    RIGHT = 2
 
-Example 1:
-Input: root = [2,1,3]
-Output: true
 
-Example 2:
-Input: root = [5,1,4,null,null,3,6]
-Output: false
-Explanation: The root node's value is 5 but its right child's value is 4.
+@unique
+class State(IntEnum):
+    DOING = 0
+    DONE = 1
+    INVALID = 2
 
-Constraints:
-* The number of nodes in the tree is in the range [1, 104].
-* -2^31 <= Node.val <= 2^31 - 1
-"""
-# TODO
+
+@dataclass(frozen=True)
+class FSM:
+    position: Position
+    root: Optional[int]
+    remaining: Tuple[int, ...]
+
+    @staticmethod
+    def split(
+            seq: Tuple[int,
+                       ...]) -> Tuple[int, Tuple[int, ...], Tuple[int, ...]]:
+        root = seq[0]
+        seq_ = seq[1:]
+        assert len(seq_) % 2 == 0
+        length = len(seq_) // 2
+
+        return root, seq_[:length], seq_[length:]
+
+    @property
+    def invalid(self) -> bool:
+        if self.position == Position.ROOT:
+            return False
+        if self.position == Position.LEFT:
+            seq = filter(lambda n: n is not None and n > self.root,
+                         self.remaining)
+        else:
+            seq = filter(lambda n: n is not None and n < self.root,
+                         self.remaining)
+        if len(tuple(seq)) >= 1:
+            return True
+        return False
+
+    @property
+    def state(self) -> State:
+        if not self.remaining:
+            return State.DONE
+        if len(self.remaining) % 2 == 0:
+            return State.INVALID
+        if self.invalid:
+            return State.INVALID
+        return State.DOING
+
+    def explode(self) -> Tuple[FSM, FSM]:
+        assert self.state == State.DOING
+        root, left, right = self.split(self.remaining)
+        return FSM(Position.LEFT, root, left), FSM(Position.RIGHT, root, right)
+
+
+class ValidateBinarySearchTree:
+    @classmethod
+    def looper(cls, inputs: Tuple[FSM, ...]) -> Optional[FSM]:
+        if not inputs:
+            return None
+        fsm = inputs[0]
+        if fsm.state == State.INVALID:
+            return fsm
+        if fsm.state == State.DONE:
+            return cls.looper(inputs[1:])
+        return cls.looper((*fsm.explode(), *inputs[1:]))
+
+    def solution(self, seq: Tuple[int, ...]):
+        fsm = FSM(Position.ROOT, None, seq)
+        res = self.looper((fsm, ))
+        if not res:
+            return True
+        return False
+
+
+if __name__ == '__main__':
+    ipt_1 = (2, 1, 3)
+    exp_1 = True
+    ipt_2 = (5, 1, None, None, 4, 3, 6)
+    exp_2 = False
+
+    # print(FSM.split(ipt_1))
+    # print(FSM.split(ipt_2))
+
+    vbst = ValidateBinarySearchTree()
+    assert exp_1 == vbst.solution(ipt_1)
+    assert exp_2 == vbst.solution(ipt_2)

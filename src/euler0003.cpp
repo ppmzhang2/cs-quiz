303fbab99ed970e0833e20e940bb5ae20f2cdd34
ZHANG Meng <ztz2000@gmail.com>
2021-11-22 19:14:07 +0800

    Revise Euler 0003
    - add cpp solution
    - minor fixes

--- src/euler0003.cpp
+++ src/euler0003.cpp
@@ -1,32 +1,34 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <vector>
 
 #define N_MAX 2000
 #define NUM 600851475143
 #define FACTOR 775146
 
-char primes[FACTOR];
+// boolean vecter in cpp uses **bitmap**
+// memory efficiency is high!
+std::vector<bool> primes (FACTOR, true);
 
-void eratosthenes_sieve (char[], unsigned long);
+void eratosthenes_sieve (std::vector<bool>& array, unsigned long);
 
 int main () {
     eratosthenes_sieve (primes, FACTOR);
     for (unsigned long i = 0; i <= FACTOR; i++) {
-        if (primes[i] == 1 && NUM % i == 0) {
+        if (primes[i] && NUM % i == 0) {
             printf ("%ld\n", i);
         }
     }
     return 0;
 }
 
-void eratosthenes_sieve (char array[], unsigned long limit) {
-    memset (array, 1, limit);
-    array[0] = array[1] = 0;
+void eratosthenes_sieve (std::vector<bool>& array, unsigned long limit) {
+    array[0] = array[1] = false;
     for (int i = 2; i <= sqrt (limit); i++) {
         if (array[i] == 1) {
             for (int j = i; j * i <= limit; j++) {
-                array[j * i] = 0;
+                array[j * i] = false;
             }
         }
     }
